5b4e70317ca1c92e4a96d7e02b293cb327616903
NAVIUOC <2019t00412@stu.cmb.ac.lk>
2022-02-07 21:30:00 +0530

    variable name changes

--- src/Carparking.java
+++ src/Carparking.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Carparking {
     public static void main(String argu[])
     {
-        Scanner sc=new Scanner(System.in);
-        Parking pa=new Parking();
+        Scanner scobj =new Scanner(System.in);
+        Parking paobj =new Parking();
         int choice;
         int car=0,bike=0,van=0,other=0;
         while (true)
@@ -16,37 +16,37 @@ public class Carparking {
             System.out.println("Press 5 to view data");
             System.out.println("Press 6 to delete data");
             System.out.println("Press 7 to exit");
-            choice=sc.nextInt();
+            choice=scobj.nextInt();
             switch (choice)
             {
                 case 1:
                     car++;
-                    pa.setCar(car);
+                    paobj.setCar(car);
                     System.out.println(car+" cars is added");
                     break;
                 case 2:
                     bike++;
-                    pa.setBike(bike);
+                    paobj.setBike(bike);
                     System.out.println(bike+" bikes is added");
                     break;
                 case 3:
                     van++;
-                    pa.setvan(van);
+                    paobj.setvan(van);
                     System.out.println(van+" van is added");
                     break;
                 case 4:
                     other++;
-                    pa.setother(other);
+                    paobj.setother(other);
                     System.out.println(other+" other is added");
                     break;
                 case 5:
-                    System.out.println("Cars: "+pa.getCar());
-                    System.out.println("Bikes: "+pa.getBike());
-                    System.out.println("van: "+pa.getvan());
-                    System.out.println("other: "+pa.getother());
+                    System.out.println("Cars: "+paobj.getCar());
+                    System.out.println("Bikes: "+paobj.getBike());
+                    System.out.println("van: "+paobj.getvan());
+                    System.out.println("other: "+paobj.getother());
                     break;
                 case 6:
-                    pa.delete();
+                    paobj.delete();
                     System.out.println("All record is deleted");
                     break;
                 case 7:
